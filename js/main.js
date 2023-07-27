af297c9411644392a1741492955aeefeb6e17211
Burhan <ahmetilov20@gmail.com>
2023-07-28 00:22:06 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,50 @@
-const name = "John";
-const age = 30;
-const rating = 4.5;
-const isCool = true;
-const x = null;
-const y = undefined;
-let z;
+/* //alert("Hello World");
+console.log("Hello World");
+console.error("This is an error");
+console.warn("This is a warning"); */
 
-console.log(typeof name);
+
+/* //var, let, const
+let age=30;
+age=31
+console.log(age);
+
+const age=30;
+age=31;
+console.log(age); */
+
+
+/* //String, Numbers, Boolean, null, undefined
+const name="John";
+const age=30;
+const rating=4.5;
+const isCool=True;
+const x=null;
+const y=undefined;
+let z;
+console.log(typeof z); */
+
+
+/* //Concatenation
+const name="John";
+const age=30;
+console.log("My name is " + name + " and I am " + age);
+//Template
+console.log(`My name is ${name} and I am ${age}`);
+const hello=`My name is ${name} and I am ${age}`;
+console.log(hello); */
+
+
+/* const s="Hello World!";
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.substring(0,5).toUpperCase());
+console.log(s.split(""));
+
+const s="technology, computers, it, code";
+console.log(s.split(", ")); */
+
+
+//Arrays
+
