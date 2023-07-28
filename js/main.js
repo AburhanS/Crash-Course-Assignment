20fbd7639c92ef1bc10343a7fdcf7503baf42ff3
Burhan <ahmetilov20@gmail.com>
2023-07-28 05:28:46 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+
+
+
 /* //alert("Hello World");
 console.log("Hello World");
 console.error("This is an error");
@@ -66,7 +69,52 @@ console.log(fruits[1]);
 console.log(Array.isArray (fruits));
 console.log(fruits.indexOf("oranges"));
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const data=[1, "apples", true];
 console.log(data); */
+
+
+const person={
+    firstName: "John",
+    lastName: "Doe",
+    age:30,
+    hobbies:["music", "movies", "sports"],
+    address:{
+        street:"50 main st",
+        city:"Boston",
+        state:"MA"
+    }
+}
+console.log(person);
+console.log(person.firstName);
+console.log(person.firstName, person.lastName);
+console.log(person.hobbies[1]);
+console.log(person.address.city);
+
+const {firstName, lastName} = person;
+console.log(firstName);
+
+const {firstName, lastName, address:{city}} =person;
+console.log(city);
+
+person.email="john@gmail.com";
+console.log(person);
 
 
